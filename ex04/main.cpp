17275b5f7e74a054c6b79b56d1ed2015beaf78ca
Rayan Mehadjeri <rmehadje@3-B-4.42heilbronn.de>
2024-05-21 16:38:29 +0200

    ex04 done, it looks perfect imo

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rmehadje <rmehadje@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/20 14:48:30 by rmehadje          #+#    #+#             */
-/*   Updated: 2024/05/21 13:07:51 by rmehadje         ###   ########.fr       */
+/*   Updated: 2024/05/21 16:38:02 by rmehadje         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,11 +25,21 @@ int main(int argc, char **argv)
 		std::cerr << "error: cannot take empty characters as arguemnts" << std::endl;
 		return 1;
 	}
+	if (s1.length() > 1)
+	{
+		std::cerr << "please make sure you only write one character as argument" << std::endl;
+		return 1;
+	}
 	else
 	{
 		std::string	s2 = std::string(argv[3]);
 		if (s1 == s2){
 			std::cerr << "nothing to be changed here" << std::endl;
+			return 1;
+		}
+		if (s2.length() > 1)
+		{
+			std::cerr << "please make sure you only write one character as argument" << std::endl;
 			return 1;
 		}
 		std::ifstream	infile(argv[1]);
@@ -44,6 +54,7 @@ int main(int argc, char **argv)
 			every += lines;
 			if (every.find(s1) == std::string::npos){
 				std::cerr << "error: cannot find the character '" << s1 << "'" << std::endl;
+				infile.close();
 				return 1;
 			}
 			if (infile.eof())
@@ -51,6 +62,12 @@ int main(int argc, char **argv)
 			every += '\n';
 		}
 		std::ofstream	outfile(std::string (argv[1]) + ".replace");
+		if (!outfile){
+			std::cerr << "error: cannot open file" << std::endl;
+			infile.close();
+			outfile.close();
+			return 1;
+		}
 		int i = every.find(s1);
 		while (i < every.length()){
 				every.erase(i, s1.length());
@@ -58,6 +75,8 @@ int main(int argc, char **argv)
 				i = every.find(s1);
 			}
 		outfile << every;
+		infile.close();
+		outfile.close();
 	}
 	return (0);
 }
